9158de2c2233b9c462074b30bda483e82bd91109
AntonioDattolo <antonio19794.a@gmail.com>
2024-04-29 14:52:34 +0200

    Prima traccia

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-let parola = [`ciao`].split()
-// let contrario = parola.reverse()
-// parola.split()
-parola.reverse();
+// **********************************************PALINDROMA
+// Chiedere all’utente di inserire una parola.
+// Creare una funzione per capire se la parola inserita è palindroma.
+// Sono accettate solo soluzioni che fanno uso del ciclo.
+// Eventuali soluzioni più "comode" potete aggiungere come extra in un commit separato solo DOPO.
+// ***************************************************
+let inputUtente = prompt("inserisci");
+// const x = inputUtente.split("");
+// const parolaDivisa = x.reverse()
+// const y = parolaDivisa.join('')
 
-console.log(parola[2])
-for(x=0; x < 2; x++){
 
+// function palindromaTest(x,y){
+//     if (inputUtente === y){
+//         console.log("è palindroma")
+//     }  else  {
+//          console.log("non è palindroma")
+//     }
+// }
+// let risultato  = palindromaTest()
+    
+
+let carattere;
+let parolaInversa= '';
+let i = inputUtente.length - 1
+while(i >= 0){
+    carattere = inputUtente[i];
+    parolaInversa += carattere; 
+    // N.B. senza il + uguale non funge
+    i--;
+    console.log(carattere)
+}
+if (inputUtente === parolaInversa){
+    console.log("palindroma")
+}else {
+    console.log("no palindroma")
 }
