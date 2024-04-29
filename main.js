a799edb2c16bb043c7dc382459c0b7d0b9d8f013
AntonioDattolo <antonio19794.a@gmail.com>
2024-04-29 16:41:58 +0200

    Traccia 2

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 // Sono accettate solo soluzioni che fanno uso del ciclo.
 // Eventuali soluzioni più "comode" potete aggiungere come extra in un commit separato solo DOPO.
 // ***************************************************
-let inputUtente = prompt("inserisci");
+// let inputUtente = prompt("inserisci");
 // const x = inputUtente.split("");
 // const parolaDivisa = x.reverse()
 // const y = parolaDivisa.join('')
@@ -20,18 +20,66 @@ let inputUtente = prompt("inserisci");
 // let risultato  = palindromaTest()
     
 
-let carattere;
-let parolaInversa= '';
-let i = inputUtente.length - 1
-while(i >= 0){
-    carattere = inputUtente[i];
-    parolaInversa += carattere; 
-    // N.B. senza il + uguale non funge
-    i--;
-    console.log(carattere)
+// let carattere;
+// let parolaInversa= '';
+// let i = inputUtente.length - 1
+// while(i >= 0){
+//     carattere = inputUtente[i];
+//     parolaInversa += carattere; 
+//     // N.B. senza il + uguale non funge
+//     i--;
+//     console.log(carattere)
+// }
+// if (inputUtente === parolaInversa){
+//     console.log("palindroma")
+// }else {
+//     console.log("no palindroma")
+// }
+
+
+// **************************************************PARI E DISPARI
+// L’utente sceglie su cosa puntare: pari o dispari.
+// L'utente inserisce un numero da 1 a 5.
+// Il computer estrae un numero casuale sempre tra 1 e 5 (usando una funzione).
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
+// Dichiariamo chi ha vinto.
+// ***************************************************
+
+let numeroUtente;
+let numeroPc ;
+let somma = `${numeroUtente}` + `${numeroPc}`;
+let esito;
+document.getElementById("play").addEventListener("click", play);
+
+
+function play(somma){
+    numeroUtente = parseInt(document.getElementById("userSlt").value);
+    // let numeroUtente = parseInt( prompt())
+    console.log(`il numero scelto dall'utente è : ${numeroUtente}`);
+    numeroPc = Math.floor(Math.random() * 6);
+    console.log(`il numero scelto dal pc è : ${numeroPc}`);
+    // console.log(`la somma dei numeri è: ${somma}`)
+    
+    // if (numeroUtente > 5){
+    //    esito = "inserisci un numero compres0 tra 0 e 5"  
+    //    console.log("il numero sceelto deve essere compreso da 0 e 5");
+    //    return esito
+    // }
+    if (somma % 2 == 0){
+       esito = "la somma è pari" 
+       console.log("il numero è pari");
+       return esito
+    }else if(somma % 2 != 0) {
+       esito = "la somma è dispari"  
+       console.log("il numero è dispari");
+       return esito
+    }
+    
 }
-if (inputUtente === parolaInversa){
-    console.log("palindroma")
-}else {
-    console.log("no palindroma")
-}
+
+esito = play(somma)
+document.getElementById("vincitore").innerHTML = esito 
+
+
+
+
